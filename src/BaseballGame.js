a4bc60b5502133098175fee80cc327fad9e0c9fe
gyeongza <gyeongza@naver.com>
2022-12-15 00:57:07 +0900

    feat : 입력한 숫자와 랜덤 숫자를 비교하는 기능 구현(재구현)

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -2,6 +2,7 @@ const InputCheck = require("./InputCheck");
 const InputView = require("./inputView");
 const OutputView = require("./OutputView");
 const RandomNumbers = require("./RandomNumbers");
+const CompareNumbers = require("./CompareNumbers");
 
 class BaseballGame {
   #computer;
@@ -31,7 +32,14 @@ class BaseballGame {
     this.#user = numbers.split("").map((number) => {
       return parseInt(number, 10);
     });
+
+    this.compare();
   };
+
+  compare() {
+    const result = new CompareNumbers().compare(this.#computer, this.#user);
+    console.log(result.ball, result.strike);
+  }
 }
 
 module.exports = BaseballGame;
